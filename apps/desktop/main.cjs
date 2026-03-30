c6778dbe7526749c957cb95e8a13d94ea6cdae9d
stanley <stanleygomesdasilva@gmail.com>
2026-03-29 21:06:52 -0300

    feat: config base electron

---
+++ apps/desktop/main.cjs
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.cjs')
+    },
+  });
+
+  const url = isDev 
+    ? 'http://localhost:3001' 
+    : `file://${path.join(__dirname, '../web-ui/out/index.html')}`;
+
+  win.loadURL(url);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
