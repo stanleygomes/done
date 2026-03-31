53250c9e53db786bc4eafdcd75cec64cec4a9911
stanley <stanleygomesdasilva@gmail.com>
2026-03-31 10:00:42 -0300

    chore: migrando npm para pnpm

--- apps/ai-api/src/config/pino.logger.ts
+++ apps/ai-api/src/config/pino.logger.ts
@@ -1,10 +1,12 @@
+import { Logger as PinoLoggerInstance } from "pino";
+
 import { Logger } from "@done/node-utils";
 import { config } from "./environment.js";
 
 export class PinoLogger {
   private static instance = Logger.getLogger(config.logger, "core-ai-api");
 
-  static getLogger() {
+  static getLogger(): PinoLoggerInstance {
     return PinoLogger.instance;
   }
 }
